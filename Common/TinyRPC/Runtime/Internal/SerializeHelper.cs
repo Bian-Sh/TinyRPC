314ac4a285083343c7ef32d0b7f0df4ac81ef0c3
边上海 <bshsf@qq.com>
2023-10-14 06:11:43 +0800

    [refact] SerrializeHelper 不需要使用泛型 T

--- Common/TinyRPC/Runtime/Internal/SerializeHelper.cs
+++ Common/TinyRPC/Runtime/Internal/SerializeHelper.cs
@@ -5,7 +5,7 @@ namespace zFramework.TinyRPC
 {
     public static class SerializeHelper
     {
-        internal static byte[] Serialize<T>(T message) where T : IMessage
+        internal static byte[] Serialize(IMessage message)
         {
             // 将消息包装
             var wrapper = new MessageWrapper
@@ -16,12 +16,12 @@ namespace zFramework.TinyRPC
             var json = JsonUtility.ToJson(wrapper);
             return System.Text.Encoding.UTF8.GetBytes(json);
         }
-        internal static T Deserialize<T>(byte[] bytes) where T : Message
+        internal static IMessage Deserialize(byte[] bytes)
         {
             // 使用 JsonUtility 反序列化
             var json = System.Text.Encoding.UTF8.GetString(bytes);
             var wrapper = JsonUtility.FromJson<MessageWrapper>(json);
-            return wrapper.Message as T;
+            return wrapper.Message;
         }
     }
 }
