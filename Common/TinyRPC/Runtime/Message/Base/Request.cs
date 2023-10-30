7fc3429854691a86699439eb466ce111ccf4200f
雨落随风 <bshsf@qq.com>
2023-10-30 20:02:35 +0800

    [update] 为 request .response 新增 tostring 方法，方便调试输出

--- Common/TinyRPC/Runtime/Message/Base/Request.cs
+++ Common/TinyRPC/Runtime/Message/Base/Request.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Diagnostics;
+using UnityEngine;
 
 namespace zFramework.TinyRPC.Messages
 {
@@ -6,11 +8,14 @@ namespace zFramework.TinyRPC.Messages
     [ResponseType(typeof(Response))]
     public class Request : IRequest
     {
+        // todo : code analysis, if found user try to modify this value, throw exception as this memeber is for internal use only
+        // as well as the Id property !
         public int id;
         public int timeout = 5000;
         /// <inheritdoc/>
         public int Id { get => id; set => id = value; }
         /// <inheritdoc/>
         public int Timeout { get => timeout; set => timeout = value; }
+        public override string ToString() => JsonUtility.ToJson(this);
     }
 }
--- Common/TinyRPC/Runtime/Message/Base/Response.cs
+++ Common/TinyRPC/Runtime/Message/Base/Response.cs
@@ -1,9 +1,10 @@
 using System;
+using UnityEngine;
 
 namespace zFramework.TinyRPC.Messages
 {
     [Serializable]
-    public class Response :  IResponse
+    public class Response : IResponse
     {
         public string error;
         public int id;
@@ -11,5 +12,6 @@ namespace zFramework.TinyRPC.Messages
         public int Id { get => id; set => id = value; }
         /// <inheritdoc/>
         public string Error { get => error; set => error = value; }
+        public override string ToString() => JsonUtility.ToJson(this);
     }
 }
