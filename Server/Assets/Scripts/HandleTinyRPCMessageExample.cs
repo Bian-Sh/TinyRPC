1a153194ccc47c615e2254be00ec52e1ff81623d
雨落随风 <bshsf@qq.com>
2023-10-30 20:16:00 +0800

    [update] 更新 TestServer 示例，新增对 Attribute 标记的消息处理器的有效性测试

--- Server/Assets/Scripts/HandleTinyRPCMessageExample.cs
+++ Server/Assets/Scripts/HandleTinyRPCMessageExample.cs
@@ -1,13 +1,8 @@
 using EasyButtons;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
 using UnityEngine;
 using zFramework.TinyRPC;
 using zFramework.TinyRPC.Generated;
-using zFramework.TinyRPC.Messages;
 using static zFramework.TinyRPC.MessageManager;
 
 public class HandleTinyRPCMessageExample : MonoBehaviour
@@ -29,7 +24,7 @@ public class HandleTinyRPCMessageExample : MonoBehaviour
         Debug.Log($"获取到{(session.IsServerSide ? "客户端" : "服务器")}  {session}  的消息, message = {message}");
     }
 
-    //[MessageHandler(MessageType.RPC)] // 与 AddNetworkSignal<Request,Response>  二选一
+    //[MessageHandler(MessageType.RPC)] // 与 AddNetworkSignal<Request,Response>  只能二选一
     private static async Task RPCMessageHandler(Session session, TestRPCRequest request, TestRPCResponse response)
     {
         Debug.Log($"{nameof(HandleTinyRPCMessageExample)}: Receive {session} request {request}");
@@ -38,35 +33,11 @@ public class HandleTinyRPCMessageExample : MonoBehaviour
     }
 
     [Button]
-    private static void RegistGeneratedMessageHandlers()
+    public static void RegistHandlerTask()
     {
-        Dictionary<Type, IRpcMessageHandler> RpcMessageHandlers = new();
         RegistRPCMessagePairs();
-        // regist rpc message handlers
-        var types = Assembly.Load("com.zframework.tinyrpc.generated")
-            .GetTypes()
-            .Where(type => type.IsSubclassOf(typeof(Request)));
-        Debug.Log($"{nameof(MessageManager)}: types coutnt = {types.Count()}");
-        // log types
-        foreach (var type in types)
-        {
-            Debug.Log($"{nameof(MessageManager)}: type is {type}");
-        }
-        // use reflection to regist rpc message handlers
-        foreach (var type in types)
-        {
-            Debug.Log($"{nameof(MessageManager)}: type is {type}");
-            var handler = Activator.CreateInstance(typeof(RpcMessageHandler<,>).MakeGenericType(type, GetResponseType(type))) as IRpcMessageHandler;
-            //RpcMessageHandlers.Add(type, handler);
-            RpcMessageHandlers[type] = handler;
-        }
-
-        // log these handlers
-        foreach (var handler in RpcMessageHandlers)
-        {
-            Debug.Log($"{nameof(MessageManager)}: {handler.Key}  {handler.Value}");
-        }
-
+        RegistGeneratedMessageHandlers();
+        RegistAttributeMarkedHandlerTask();
     }
 
 }
--- Server/Assets/Scripts/TestRpcServerProvider.cs
+++ Server/Assets/Scripts/TestRpcServerProvider.cs
@@ -5,15 +5,24 @@ namespace zFramework.TinyRPC.Example
 {
     //todo: 将要通过代码生成的方式自动的
     // 现在这个 RPC 消息还不能使用，因为没有注册到 RpcMessageHandlers 中
+    [MessageHandlerProvider]
     public class TestRpcServerProvider
     {
         [MessageHandler(MessageType.RPC)]
         static Task OnLoginRequest(Session session, C2S_Login request, S2C_Login response)
         {
+            Debug.Log($"{nameof(TestRpcServerProvider)}:  Attribute marked RPC Handler receive  message correctly, for more info see below!");
             Debug.Log($"{nameof(TestRpcServerProvider)}: Receive RPC Request ：{request.name} ");
             response.success = true;
             response.token = "RPC Server Say Login Success";
             return Task.FromResult(response);
+        }
+
+        [MessageHandler(MessageType.Normal)]
+        static void OnAttributeRegistTestMessage(Session session, AttributeRegistTestMessage message)
+        {
+            Debug.Log($"{nameof(TestRpcServerProvider)}:  Attribute marked  NORMAL Message Handler receive  message correctly, for more info see below!");
+            Debug.Log($"{nameof(TestRpcServerProvider)}: Receive Normal Message ：{message.desc} ");
         }
     }
 }
