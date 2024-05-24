7e3d735ac91e96c25fb84c95afaf7011e8762534
边上海 <bshsf@qq.com>
2024-05-24 19:45:03 +0800

    [fixed] 冗余的对象池分配，理由是具体message的对象池处理发生在 Message Wrapper 内

--- Common/TinyRPC/Runtime/Data/SerializeHelper.cs
+++ Common/TinyRPC/Runtime/Data/SerializeHelper.cs
@@ -36,8 +36,6 @@ namespace zFramework.TinyRPC
             var json = System.Text.Encoding.UTF8.GetString(bytes);
             // MessageWrapper 池化
             var wrapper = Allocate<MessageWrapper>();
-            //具体的网络消息 池化，框架内的 Ping 由框架回收，框架外的消息需要用户手动回收
-            wrapper.Message = Allocate<IMessage>();
             // 使用 JsonUtility 反序列化
             JsonUtility.FromJsonOverwrite(json, wrapper);
             var message = wrapper.Message;
