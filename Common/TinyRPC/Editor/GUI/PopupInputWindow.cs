f89ccd1bb697e0dcd6b50625dbb96e4f77e42af3
雨落随风 <bshsf@qq.com>
2024-07-04 01:48:16 +0800

    [update] 新增弹窗输入 proto 文件名的弹窗组件

---
+++ Common/TinyRPC/Editor/GUI/PopupInputWindow.cs
@@ -0,0 +1,77 @@
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+namespace zFramework.TinyRPC.Editors
+{
+    /// <summary>
+    ///  弹窗输入文件名
+    /// </summary>
+    public class PopupInputWindow : EditorWindow
+    {
+        private string protoName;
+        private bool isDuplicated;
+        private Rect rect;
+        private readonly TaskCompletionSource<string> tcs;
+        static PopupInputWindow instance;
+        private void OnEnable() => instance = this;
+        public PopupInputWindow() => tcs = new TaskCompletionSource<string>();
+
+        public void OnGUI()
+        {
+            rect.width = 160;
+            rect.height = 48;
+            GUILayout.Space(5);
+            protoName = EditorGUILayout.TextField("", protoName);
+            var lastRect = GUILayoutUtility.GetLastRect();
+            if (lastRect.Contains(Event.current.mousePosition))
+            {
+                EditorGUI.LabelField(lastRect, inputContent);
+            }
+            var isEmpty = string.IsNullOrEmpty(protoName);
+            var isExclude = protoName == TinyRpcEditorSettings.ProtoFileContainer;
+            if (isExclude)
+            {
+                EditorGUILayout.HelpBox("此文件名不被允许！", UnityEditor.MessageType.Error);
+                rect.height += 38;
+            }
+            if (isDuplicated)
+            {
+                EditorGUILayout.HelpBox("此文件名已存在！", UnityEditor.MessageType.Error);
+                rect.height += 38;
+            }
+            using var horzScope = new EditorGUILayout.HorizontalScope();
+            using var disableScope = new EditorGUI.DisabledScope(isEmpty || isDuplicated || isExclude);
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button("确定", GUILayout.Width(80)))
+            {
+                tcs.SetResult(protoName);
+                this.Close();
+            }
+            GUILayout.FlexibleSpace();
+            this.position = rect;
+            this.minSize = new Vector2(160, rect.height);
+        }
+
+        public static Task<string> WaitForInputAsync(TinyRpcEditorSettings settings, Rect rect)
+        {
+            if (!instance)
+            {
+                instance = CreateInstance<PopupInputWindow>();
+                instance.rect = rect;
+                // calc if it is duplicated
+                instance.isDuplicated = settings.protos.Any(asset => asset.file && asset.file.name == instance.protoName);
+            }
+            instance.ShowPopup();
+            instance.Focus();
+            return instance.tcs.Task;
+        }
+        // 当 PopupInputWindow 丢失焦点，窗口关闭
+        private void OnLostFocus()
+        {
+            tcs.SetResult(null);
+            this.Close();
+        }
+        readonly GUIContent inputContent = new("", "请输入文件名！");
+    }
+}
